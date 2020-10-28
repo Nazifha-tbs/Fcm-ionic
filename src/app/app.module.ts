13a73377e911eb3c994dfdae1e47592b0d556af3
Nazifha Mansuri <tbs.nazifha@gmail.com>
2020-10-28 12:23:52 +0530

    Push notification initial implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+// import { FCM } from './../../plugins/cordova-plugin-fcm-with-dependecy-updated/src/ionic/ngx/FCM';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -8,14 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HttpClient,HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
+],
   providers: [
+    // FCM,
     StatusBar,
     SplashScreen,
+    HttpClient,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,118 @@
+import { FCM } from './../../../plugins/cordova-plugin-fcm-with-dependecy-updated/src/ionic/FCM';
+import { Platform } from '@ionic/angular';
+// import { FCM } from './../../../plugins/cordova-plugin-fcm-with-dependecy-updated/src/ionic/ngx/FCM';
 import { Component } from '@angular/core';
-
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from "@angular/common/http";
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public hasPermission: boolean;
+  public token: any;
+  public receiverToken:any;
+  public message: any;
+  public notificationUrl = "https://fcm.googleapis.com/fcm/send";
+  constructor(
+    // public fcm: FCM, 
+    public platform: Platform, private http: HttpClient) {
+    this.setupFCM();
+  }
+  private async setupFCM() {
+    await this.platform.ready();
 
-  constructor() {}
+    console.log('FCM SETUP INIT');
+    if (!this.platform.is('cordova')) {
+      return;
+    }
 
+    console.log('IN CORDOVA');
+    this.hasPermission = await FCM.requestPushPermission();
+    console.log('CHECK hasPermission:', this.hasPermission);
+
+
+    // this.token = await this.fcm.getToken();
+    this.token = await FCM.getToken()
+
+    console.log('CHECK getToken: ' + this.token);
+
+    console.log('ON NOTIFICATION SUBSCRIBE');
+    // this.fcm
+    //   .onTokenRefresh()
+    //   .subscribe((newToken) => console.log('NEW TOKEN:', newToken));
+    FCM
+      .onNotification()
+      .subscribe((payload: object) => {
+        console.log('ON NOTIFICATION:', payload)
+        alert(JSON.stringify(payload));
+      });
+  }
+  // sendMsg(val){
+  //   let body = {
+  //     "notification": {
+  //       "title": "New Notification has arrived",
+  //       "body": "Notification Body",
+  //       "sound": "default",
+  //       "click_action": "FCM_PLUGIN_ACTIVITY",
+  //       "icon": "fcm_push_icon"
+  //     },
+  //     "data": {
+  //       "param1": "value1",
+  //       "param2": "value2"
+  //     },
+  //     "to": "/topics/all",
+  //     "priority": "high",
+  //     "restricted_package_name": ""
+  //   }
+  //   // let options = new HttpHeaders().set('Content-Type', 'application/json');
+  //   // this.http.post(this.notificationUrl, body, {
+  //   //   headers: options.set('Authorization', this.token),
+  //   // })
+  //   //   .subscribe();
+  // }
+  sendMsg() {
+    
+    let body = {
+      
+      "notification": {
+        "title": "You have new Message!",
+        "body": this.message,
+        "sound": "default",
+        "click_action": "FCM_PLUGIN_ACTIVITY",
+        "icon": "fcm_push_icon"
+      },
+      data: {
+        param1: this.message,
+      },
+      alert:{
+        "title": "You have new Message!",
+        "body": this.message, 
+      },
+      "content_available": true,
+      "priority": "high",
+      to: 'dpuzTxs_DFs:APA91bFc2lshv6sDVvddUVe-LWe0VmHbMJJ2KIarNe6RpM-3kSlcyN6el12Vau6UOgzij4e9qUXhvrP1ppyO7Nd2X-mfMp78_82B0t3ZpNAN6i5xq4rEltVJzD_mJaLtR3u5TCNaGkW7'
+    };
+    this.http
+      .post(this.notificationUrl, body, {
+        headers: {
+          Authorization:
+            "key=AAAAlmUJzJU:APA91bGeoTH0SySDoi4GxdgJfUzzawZn_dh8Pu7eFc-4mX1l9prO3F5aVEsb7Znj34V5KsHrb2qiEXRU-CwpPSDfWMuElh2T_sBkPVT01v66IhTnZ28LbrjmkekVFtcgI3vet5ZUixBB",
+          "Content-Type": "application/json",
+        },
+      })
+      .subscribe((response: any) => {
+        console.log("Notification response:", response);
+      });
+    // FCM
+    //   .onNotification()
+    //   .subscribe((payload: object) => {
+    //     alert(payload)
+    //     console.log('ON NOTIFICATION:', payload)
+    //   });
+  }
 }
